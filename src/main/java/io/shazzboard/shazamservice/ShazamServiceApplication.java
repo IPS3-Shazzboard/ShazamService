0823de263f8ea9401b9528b0806e4c2620e574e3
Calvin Zhen <418224@student.fontys.nl>
2022-10-10 15:05:53 +0200

    Initial commit

---
+++ src/main/java/io/shazzboard/shazamservice/ShazamServiceApplication.java
@@ -0,0 +1,13 @@
+package io.shazzboard.shazamservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShazamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShazamServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/shazzboard/shazamservice/ShazamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shazzboard.shazamservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShazamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
