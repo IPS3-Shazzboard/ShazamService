a2e0f714fd022066e2692f4e1a02edc6ef947012
Calvin Zhen <418224@student.fontys.nl>
2022-10-17 09:36:42 +0200

    Removed test for testing CI/CD

--- src/test/java/io/shazzboard/shazamservice/ShazamServiceApplicationTests.java
+++ src/test/java/io/shazzboard/shazamservice/ShazamServiceApplicationTests.java
@@ -1,13 +1,24 @@
 package io.shazzboard.shazamservice;
 
+import io.shazzboard.shazamservice.service.ShazamService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ShazamServiceApplicationTests {
+import static org.hamcrest.MatcherAssert.assertThat;
 
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ShazamServiceApplicationTests {
+//	@Autowired
+//	private ShazamService shazamService;
+//
+//	@Test
+//	public void serviceExists() throws Exception {
+//		assertThat(shazamService).isNotNull();
+//	}
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
