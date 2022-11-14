ac508cf9aa18dacd262174a6d4cc819eeac81369
Calvin Zhen <418224@student.fontys.nl>
2022-11-14 13:03:08 +0100

    Removed empty test

--- src/test/java/io/shazzboard/shazamservice/ShazamServiceApplicationTests.java
+++ src/test/java/io/shazzboard/shazamservice/ShazamServiceApplicationTests.java
@@ -1,26 +1,13 @@
 package io.shazzboard.shazamservice;
 
-//import io.shazzboard.shazamservice.service.ShazamService;
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
 class ShazamServiceApplicationTests {
-	//@Autowired
-	//private ShazamService shazamService;
 
 	@Test
-	public void serviceExists(){
-//        assertThat(shazamService).isNotNull();
-	    assert(true);
-    }
-
-	@Test
-	void contextLoads() {
+	public void serviceExists() {
+		assert (true);
 	}
-
 }
